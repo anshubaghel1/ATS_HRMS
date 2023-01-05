81c55fcf407c7aa632e73b5fab55a40a84863cc5
anshubaghel1 <anshupalbhagel@gmail.com>
2023-01-05 21:16:50 +0530

    registration code added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
 
 const routes: Routes = [
-  {path: '' , component: LoginComponent}
+  {path: '' , component: LoginComponent},
+  {path: 'Registration' , component: RegistrationComponent}
 ];
 
 @NgModule({
